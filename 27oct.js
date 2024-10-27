fb407d2118e3805ef735074431eae6fd7de4f750
ShanzayEman <shanzayeman0@gmail.com>
2024-10-27 22:16:17 +0500

    Add files via upload

---
+++ 27oct.js
@@ -0,0 +1,27 @@
+let text;
+switch (new Date().getDay()) {
+  case 4:
+  case 5:
+    text = "Soon it is Weekend";
+    break;
+  case 0:
+  case 6:
+    text = "It is Weekend";
+    break;
+  default:
+    text = "Looking forward to the Weekend";
+}
+document.getElementById("demo1").innerHTML = text;
+let x = "0";
+
+switch (x) {
+  case 0:
+    text = "Off";
+    break;
+  case 1:
+    text = "On";
+    break;
+  default:
+    text = "No value found";
+}
+document.getElementById("demo2").innerHTML = text;
